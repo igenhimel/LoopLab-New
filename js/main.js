4907c9839c76f034cf5caaa16ecd7fd5c6117047
igenhimel <djstar.himel@gmail.com>
2021-03-29 02:25:22 +0600

    updated

--- js/main.js
+++ js/main.js
@@ -1,6 +1,41 @@
 $(document).ready(function () {
  
     new WOW().init();
+    
+   
+  $('.name').focusin(function () { 
+     
+    $('.name').siblings().addClass("magic");
       
+  });
+    
+  $('.name').focusout(function () { 
+     
+    $('.name').siblings().removeClass("magic");
+      
+  });
+  $('.email').focusin(function () { 
+     
+    $('.email').siblings().addClass("magic");
+      
+  });
+    
+  $('.email').focusout(function () { 
+     
+    $('.email').siblings().removeClass("magic");
+      
+  });
+  $('.text').focusin(function () { 
+     
+    $('.text').siblings().addClass("magic");
+      
+  });
+    
+  $('.text').focusout(function () { 
+     
+    $('.text').siblings().removeClass("magic");
+      
+  });
+
 });
 
